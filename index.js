131dc7bae7a4832cd95bd325992e245d210e194c
crispy-barnacle <83539938+crispy-barnacle@users.noreply.github.com>
2022-09-02 15:29:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Favourite courses
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  try to take over the world!
 // @author       Me
 // @match        https://courses.cs.ut.ee/courses/index/2022/fall
@@ -13,27 +13,32 @@
 
 (function() {
     'use strict';
-    
+
     // NB! Use javascript source code when using letters with diacritics or Cyrillic letters, e.g.: \u00FC
-    var courseNames = ["Eestikeelne kommunikatsioon arvutiteaduses", "Sissejuhatus andmeteadusesse", "Praktika informaatikas", "Tehisintellekt", "Programmeerimine"];
+    let courseNames = ["Eestikeelne kommunikatsioon arvutiteaduses", "Sissejuhatus andmeteadusesse", "Praktika informaatikas", "Tehisintellekt", "Programmeerimine"];
     console.log(courseNames);
 
-    var contentElement = document.getElementsByClassName("content")[0];
-    var courseListToAdd = document.createElement("ul");
+    let contentElement = document.getElementsByClassName("content")[0];
+    let courseListToAdd = document.createElement("ul");
+    let arrayOfCourses = [];
     courseListToAdd.className = "course-list";
 
-    for (var course of contentElement.getElementsByClassName("course-list")[0].children) {
-        var courseCode = course.getElementsByClassName("course-code")[0].innerText;
-        var courseName = course.innerText.replace(courseCode + "\n", "");
-        console.log(courseName);
+    for (let course of contentElement.getElementsByClassName("course-list")[0].children) {
+        let courseCode = course.getElementsByClassName("course-code")[0].innerText;
+        let courseName = course.innerText.replace(courseCode + "\n", "");
         if (courseNames.includes(courseName)) {
-            courseListToAdd.append(course);
+            arrayOfCourses.push(course);
         }
     }
+
+    for (let course of arrayOfCourses) {
+        courseListToAdd.append(course);
+    }
+
     contentElement.prepend(document.createElement("br"));
     contentElement.prepend(courseListToAdd);
 
-    var header = document.createElement("h2");
+    let header = document.createElement("h2");
     header.innerText = "Favourites";
     contentElement.prepend(header);
 })();
